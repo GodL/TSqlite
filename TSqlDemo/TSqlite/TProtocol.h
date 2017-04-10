f4205dca90b6556a5843c502d74a02c07f89bda2
godL <547188371@qq.com>
2017-04-10 21:41:18 +0800

    add TProtocol

---
+++ TSqlDemo/TSqlite/TProtocol.h
@@ -0,0 +1,18 @@
+//
+//  TProtocol.h
+//  TSqlDemo
+//
+//  Created by 李浩 on 2017/4/10.
+//  Copyright © 2017年 李浩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol TProtocol <NSObject>
+
+@optional
+
++ (NSString*)tableName;
+
+
+@end
